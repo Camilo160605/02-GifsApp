a12ee3db9a4731d83f664cd31c54009a2618b3c0
Camilo160605 <camilo1665ober@gmail.com>
2023-10-17 16:21:13 -0500

    Gifs has been added

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
+  exports: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule
   ]
